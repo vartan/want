1e61354f54f873881be2bacd4330661c7a03fc32
Michael Vartan <michael.vartan@student.csulb.edu>
2014-12-10 18:59:16 -0800

    added affiliate

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
-document.body.onload = function() {
-	$.getJSON("products.json", function(products) {
-		var container = $(".container");
-		for(var i in products) {
-			var product = products[i];
-			var url = "http://www.amazon.com/dp/"+product.ASIN+"/?tag=vartannet46ne-20"
-			container.append("<tr><td><a href=\""+url+"\" target=\"_blank\"><div class=\"lazy\" src=\"http://images.amazon.com/images/P/"+product.ASIN+".01._SCMZZZZZZZ_.jpg\"/></div></a></td><td><a href=\""+url+"\" target=\"_blank\">"+product.title+"</a></td><td>"+product.price+"</td></tr>")
-		}
-		
-	});
-} 
+
 function elementInViewport(el) {
   var top = el.offsetTop;
   var left = el.offsetLeft;
@@ -29,7 +19,7 @@ function elementInViewport(el) {
   );
 }
 
-$(window).on("load resize scroll",function(e){
+var fixImages = function() {
     $(".lazy").each(function(index, element) {
     	if(elementInViewport(element)) {
     		var $elem = $(element);
@@ -37,4 +27,16 @@ $(window).on("load resize scroll",function(e){
     		$elem.html("<img src=\""+$elem.attr("src")+"\"/>");
     	}
     })
-});
+}
+$(window).on("load resize scroll", fixImages);
+document.body.onload = function() {
+	$.getJSON("products.json", function(products) {
+		var container = $(".container");
+		for(var i in products) {
+			var product = products[i];
+			var url = "http://www.amazon.com/dp/"+product.ASIN+"/?tag=vartannet46ne-20"
+			container.append("<tr><td><a href=\""+url+"\" target=\"_blank\"><div class=\"lazy\" src=\"http://images.amazon.com/images/P/"+product.ASIN+".01._SCMZZZZZZZ_.jpg\"/></div></a></td><td><a href=\""+url+"\" target=\"_blank\">"+product.title+"</a></td><td>"+product.price+"</td></tr>")
+		}
+		fixImages();
+	});
+} 
