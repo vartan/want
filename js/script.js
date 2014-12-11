291b1fe0bbb905447ae87da6a853cb97a8833f40
Michael Vartan <michael.vartan@student.csulb.edu>
2014-12-10 18:44:20 -0800

    added affiliate

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ document.body.onload = function() {
 		var container = $(".container");
 		for(var i in products) {
 			var product = products[i];
-			container.append("<tr><td><a href=\"http://www.amazon.com/dp/"+product.ASIN+"\"><img src=\"http://images.amazon.com/images/P/"+product.ASIN+".01._SCMZZZZZZZ_.jpg\"/></a></td><td><a href=\"http://www.amazon.com/dp/"+product.ASIN+"\">"+product.title+"</a></td><td>"+product.price+"</td></tr>")
+			var url = "http://www.amazon.com/dp/"+product.ASIN+"/?tag=vartannet46ne-20"
+			container.append("<tr><td><a href=\""+url+"\"><img src=\"http://images.amazon.com/images/P/"+product.ASIN+".01._SCMZZZZZZZ_.jpg\"/></a></td><td><a href=\""+url+"\">"+product.title+"</a></td><td>"+product.price+"</td></tr>")
 		}
 		
 	});
